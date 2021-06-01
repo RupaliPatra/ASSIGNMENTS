73534679fbe5feeed5d93ca012fea96d12dfd77f
Kumbhakarnaravan <Kumbhakarna.ravan@gmail.com>
2021-06-01 13:10:41 +0530

    MY DAY 3 ASSIGNMENTS

---
+++ src/DAY_2/ArithmeticOperators.java
@@ -0,0 +1,23 @@
+package DAY_2;
+
+public class ArithmeticOperators {
+
+	public static void main(String[] args) {
+		 int a = 60, b = 12;
+
+		    // addition operator
+		    System.out.println("a + b = " + (a + b));
+
+		    // subtraction operator
+		    System.out.println("a - b = " + (a - b));
+
+		    // multiplication operator
+		    System.out.println("a * b = " + (a * b));
+
+		    // division operator
+		    System.out.println("a / b = " + (a / b));
+
+		    // modulus operator
+		    System.out.println("a % b = " + (a % b));
+		  }
+}
---
+++ src/DAY_2/HelloWorld.java
@@ -0,0 +1,10 @@
+package DAY_2;
+
+public class HelloWorld {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+	}
+
+}
